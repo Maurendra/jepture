7336cec6424104ead2c19d9fcba70e35c207fe33
maurendra <maurendra@glovory.com>
2019-12-04 17:57:41 +0700

    Project pertama

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Posts from './components/Posts';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="ui container">
+        <BrowserRouter>
+          <Fragment>
+            <Switch>
+              <Route path="/" exact component={Login} />
+              <Route path="/dashboard" exact component={Dashboard} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/post" exact component={Posts} />
+            </Switch>
+          </Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
